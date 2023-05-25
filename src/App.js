f2ade1b359e978864c5ecbd0c6bbd14318928896
swaroopsv96 <swaroopsv96@gmail.com>
2023-05-25 14:30:40 -0400

    Adding Login and App

--- src/App.js
+++ src/App.js
@@ -7,17 +7,46 @@ import Header from './components/Header';
 import Createemployee from './components/Createemployee';
 import ReactDOM from "react-dom";
 import React, { useState } from 'react';
+import Login from './Login';
 
+const App = () =>  {
+  const [loggedIn, setLoggedIn] = useState(false);
 
-function App() {
+  const handleLogin = () => {
+    // Perform login logic and authentication
+    // If login is successful, set loggedIn to true
+    setLoggedIn(true);
+    console.log('Login successful');
+  };
+
+  const handleLogout = () => {
+    // Perform logout logic
+    // Set loggedIn to false
+    setLoggedIn(false);
+    console.log('Logout successful');
+  };
+
   return (
   	<div>
     <Router>
-  	  <Header/>
+  	  <Header loggedIn={loggedIn} onLogout={handleLogout} />
         <div className="container">
         <Routes>
-          <Route exact path='/employee' element={< ListEmployees />}></Route>
-          <Route exact path='/add-employee' element={< Createemployee />}></Route>
+            <Route
+              exact
+              path="/"
+              element={loggedIn ? <Navigate to="/employee" /> : <Login onLogin={handleLogin} />}
+            />
+            <Route
+              exact
+              path="/employee"
+              element={loggedIn ? <ListEmployees /> : <Navigate to="/" />}
+            />
+            <Route
+              exact
+              path="/add-employee"
+              element={loggedIn ? <CreateEmployee /> : <Navigate to="/" />}
+            />
           </Routes>
     </div>
     </Router>
---
+++ src/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        // Login successful
+        onLogin();
+      } else {
+        // Login failed
+        console.log('Login failed');
+      }
+    } catch (error) {
+      console.log('Error occurred during login:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
